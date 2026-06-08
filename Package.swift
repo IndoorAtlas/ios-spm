ab80263558fa06bbe01f5f9717acf2aa34159d46
Matti Tiainen <matti.tiainen@indooratlas.com>
2026-06-08 15:52:19 +0300

    Update to 3.8.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IndoorAtlas",
-            url: "https://dl.cloudsmith.io/public/indooratlas/indooratlas-ios-sdk/raw/names/indooratlas-ios-sdk/versions/3.8.0/indooratlas-ios-sdk-3.8.0-release-dfa07fa-147.zip",
-            checksum: "a1f99d61a6c93d57f16261343871c81166872a28625ff9da21df76e8a452d85d"
+            url: "https://dl.cloudsmith.io/public/indooratlas/indooratlas-ios-sdk/raw/names/indooratlas-ios-sdk/versions/3.8.1/indooratlas-ios-sdk-3.8.1-release-c7543c3-149.zip",
+            checksum: "294ab533387c751d6f4432af27fce1f847ea068ec0d0c435911e039b25ad1afa"
         ),
     ],
     swiftLanguageVersions: [.v5]
